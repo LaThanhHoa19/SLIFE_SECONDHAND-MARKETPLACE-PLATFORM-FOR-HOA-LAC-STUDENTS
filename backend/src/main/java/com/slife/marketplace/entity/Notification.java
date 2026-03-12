29a354de13647fea6e5475a85ba55928afb31f12
momoPC <maid23344@gmail.com>
2026-03-13 01:42:38 +0700

    Update new db schema and update entity

--- backend/src/main/java/com/slife/marketplace/entity/Notification.java
+++ backend/src/main/java/com/slife/marketplace/entity/Notification.java
@@ -54,5 +54,10 @@ public class Notification {
     @Column(name = "created_at", nullable = false)
     private Instant createdAt;
 
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
 
 }
