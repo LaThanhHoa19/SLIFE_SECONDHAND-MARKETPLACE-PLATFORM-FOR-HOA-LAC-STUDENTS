3f4f3680a9c3917be358d932a3125db932c5a940
anhttn-he186474 <anhttnhe186474@fpt.edu.vn>
2026-03-19 17:09:11 +0700

    SCRUM - 129 [AUTH] User Auth
    Chỉnh sửa frontend của login cho đồng bộ màu với background của web

---
+++ frontend/src/components/layout/AuthLayout.jsx
@@ -0,0 +1,10 @@
+import { Box } from '@mui/material';
+import { Outlet } from 'react-router-dom';
+
+export default function AuthLayout() {
+    return (
+        <Box sx={{ minHeight: '100vh', display: 'flex', alignItems: 'center', justifyContent: 'center', bgcolor: '#1C1B23' }}>
+            <Outlet />
+        </Box>
+    );
+}
