bdec0d80c061c618b0f662a5d2f6a66cf827a591
momoPC <maid23344@gmail.com>
2026-03-23 22:56:20 +0700

    SCRUM-104: Role check ADMIN_BESCRUM-104: Role check ADMIN_BE

--- backend/src/main/java/com/slife/marketplace/config/SecurityConfig.java
+++ backend/src/main/java/com/slife/marketplace/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.slife.marketplace.security.JwtAuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -11,6 +12,7 @@ import org.springframework.security.web.authentication.UsernamePasswordAuthentic
 import org.springframework.security.config.Customizer;
 
 @Configuration
+@EnableMethodSecurity
 public class SecurityConfig {
 
   private final JwtAuthenticationFilter jwtAuthenticationFilter;
